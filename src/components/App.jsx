d7281fb787b0af336fd9421820f09c3f2bc38564
Aloysius Hon <aloysiushon@hotmail.com>
2024-02-21 22:42:47 +0800

    Add Mapping data to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import Heading from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes"
+
+console.log(notes)
+function createNotes(note) {
+  return (
+    <Note
+      key={note.id}
+      id={note.id}
+      title={note.title}
+      content={note.content}
+    />
+  )
+}
 
 function App() {
   return (
     <div>
       <Heading/>
-      <Note/>
+      {notes.map(createNotes)}
       <Footer/>
     </div>
   );
